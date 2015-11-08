454032af0834916b566059ddfba94c0adc731b57
jens-t <jens-t@users.noreply.github.com>
2015-11-08 20:00:45 +0100

    AccountStorage refactored

--- src/js/AccountStorage.js
+++ src/js/AccountStorage.js
@@ -1,72 +1,54 @@
-define([
-
-], function () {
+define([], function () {
   'use strict';
 
 	var AccountStorage = (function AccountStorage() {
-		var accounts = {};
-		var storageItemName = 'mabe_account_data';
+		var accounts = {},
+		    storageItemName = 'mabe_account_data';
 
-		function store() {
+		function write() {
 			localStorage.setItem(storageItemName, JSON.stringify(accounts));
 		}
 
-		function recover() {
-			try {
-				accounts = JSON.parse(localStorage.getItem(storageItemName));
-				if (!accounts) {
-					accounts = {};
-				}
-			}
-			catch(e) {
-				accounts = {};
-			}
+		function read() {
+			accounts = JSON.parse(localStorage.getItem(storageItemName)) || {};
 		}
 
-		function getNewID() {
-			var id = 0;
-			for (var oid in accounts) {
-				if (oid > id) {
-					id = oid;
-				}
-			}
-			id++;
-			return id;
+		function generateId() {
+      var keys = Object.keys(accounts);
+      return parseInt(keys[keys.length - 1]) + 1 || 0;
 		}
 
-		recover();
+		read();
 
 		return {
-			getAllKeys: function getAccountKeys() {
-				return Object.keys(accounts);
-			},
-			getAll: function getAccounts() {
-				return accounts;
-			},
+
 			get: function getAccount(id) {
 				return accounts[id];
 			},
-			set: function setAccount(accountData) {
-				var id = 0;
-				if (accountData.id) {
-					id = accountData.id;
+
+			set: function setAccount(account) {
+				if(account.id === undefined){
+					account.id = generateId();
 				}
-				else {
-					id = getNewID();
-					accountData.id = id;
-				}
-				accounts[id] = accountData;
-				store();
-				return id;
+				accounts[account.id] = account;
+				write();
+				return account.id;
 			},
-			del: function delAccount(id) {
+
+			delete: function deleteAccount(id) {
         delete accounts[id];
-				store();
+				write();
 			},
-			delall: function delAllAccounts() {
-				localStorage.setItem(storageItemName, null);
-				recover();
+
+      getAll: function getAccounts() {
+				return accounts;
+			},
+
+			deleteAll: function deleteAllAccounts() {
+				accounts = {};
+				write();
 			}
+
 		};
 	})();
 
