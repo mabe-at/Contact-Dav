58e430f9554241cb9d29d1a69d56a39060636802
jens-t <jens-t@users.noreply.github.com>
2015-11-22 18:49:23 +0100

    CardDavConnector  refactored & completed

--- src/js/CardDavConnector.js
+++ src/js/CardDavConnector.js
@@ -1,11 +1,9 @@
-define(['underscore', 'promise', 'dav', 'vcard'], function (_, Es6Promise, Dav, Vcard) {
+define(['lodash', 'dav', 'vcard'], function (_, Dav, Vcard){
   'use strict';
 
-  Promise = Es6Promise.Promise;
+	var CardDavConnector = (function(){
 
-	var CardDavConnector = {
-
-    getConnection: function getConnection(account, connectionType) {
+    function getConnection(account, connectionType){
       return {
         server: account.url,
         xhr: new Dav.transport.Basic(
@@ -16,25 +14,23 @@ define(['underscore', 'promise', 'dav', 'vcard'], function (_, Es6Promise, Dav, 
         ),
         accountType: 'carddav',
         loadCollections: true,
-        loadObjects: (connectionType === 'meta') ? false : true
+        loadObjects: (connectionType === 'load-objects') ? true : false
       };
-    },
+    }
 
-    getMetaConnection: function getMetaConnection(account) {
-      return CardDavConnector.getConnection(account, 'meta');
-    },
+    function getAddressbooks(accounts, connectionType){
+      return new Promise(function(resolve, reject){
 
-    getAddressbooks: function getAddressbooks(accounts){
-      return new Promise(function(resolve, reject) {
-        var connections = _.map(accounts, CardDavConnector.getMetaConnection),
+        var connections = _.map(accounts, function(account){
+              return getConnection(account, connectionType);
+            }),
             accountPromises = _.map(connections, Dav.createAccount);
 
         Promise.all(accountPromises).then(function(responses){
-          console.log(responses);
           var addressbooks = [];
           _.each(responses, function(response){
             _.each(response.addressBooks, function(addressBook){
-              if(_.indexOf(addressBook.resourcetype, 'addressbook') !== -1){
+              if(_.includes(addressBook.resourcetype, 'addressbook') === true){
                 addressbooks.push(addressBook);
               }
             });
@@ -47,101 +43,35 @@ define(['underscore', 'promise', 'dav', 'vcard'], function (_, Es6Promise, Dav, 
       });
     }
 
-  };
+    function getContacts(accounts){
+      return new Promise(function(resolve, reject){
+        getAddressbooks(accounts, 'load-objects')
+          .then(function(addressbooks){
+
+            var contacts = [];
+            addressbooks.forEach(function(addressBook){
+              addressBook.objects.forEach(function(object){
+                var contact = object.data.props.addressData;
+                Vcard.parse(contact, function(vcard){
+                  contacts.push(vcard);
+                });
+              });
+            });
+            resolve(contacts);
+
+          })
+          .catch(function(error){
+            reject(error);
+          });
+      });
+    }
+
+    return {
+      getAddressbooks: getAddressbooks,
+      getContacts: getContacts
+    };
+
+  })();
 
 	return CardDavConnector;
 });
-
-
-
-/*
-
-Dav.createAccount({
-  server: 'https://cloud.develhub.de/remote.php/carddav/addressbooks/jenst/kontakte',
-  xhr: new Dav.transport.Basic(
-    new Dav.Credentials({
-      username: 'jenst',
-      password: '17xnt5x1!Q60595025'
-    })
-  ),
-  accountType: 'carddav',
-  loadCollections: true,
-  loadObjects: true
-})
-.then(function(account) {
-  //console.log(account);
-  account.addressBooks.forEach(function(addressBook) {
-    addressBook.objects.forEach(function(object) {
-      var contact = object.data.props.addressData;
-      Vcard.VCF.parse(contact, function(vcard) {
-        console.log(vcard);
-      });
-    });
-  });
-})
-.catch(function(err) {
-  console.error('Could not load: ', err);
-});
-
- */
-
-
-
-
-
-
-
-/*
-
-var connection = this.connection,
-    addressbooks = [];
-
-callback = (typeof callback === 'function') ? callback : function() {};
-
-connection.onready = function() {
-  connection.getResource(null, function(resource, error) {
-    if (resource.xml !== null && error === undefined) {
-      _findBooks(resource, callback);
-    }
-  });
-};
-
-connection.onerror = function(error) {
-  console.log(error);
-
-  if(error === 'connection error'){
-    // check internet connection & server availability, connection error
-  }
-
-  if(error === 'No valid DAV XML Response'){
-    // check url or server, response invalid
-  }
-
-  callback(addressbooks);
-};
-
-connection.connect();
-
-function _findBooks(resource, callback){
-console.log(resource);
-  if (resource.isCollection() === true && resource.isAddressBook() === true) {
-    addressbooks.push(resource.getMetadata());
-  }
-
-  _.each(resource.getContents(), function(item){
-console.log(item.type);
-    if (item.type !== 'file') {
-      connection.getResource(item.href, function(resource, error) {
-        if (resource.xml !== null && error === undefined) {
-          _findBooks(resource, callback);
-        }
-      });
-    }
-  });
-
-  //if (bookcalls[account.url + account.user] == 0 && callback) {
-    callback(addressbooks);
-  //}
-}
-
- */
